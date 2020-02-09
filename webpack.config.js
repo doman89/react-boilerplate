c69532b38e019ca255fb9b8b25db3c78f1a0f040
Mateusz Domanski <mateusz.domanski.1989@gmail.com>
2020-02-09 13:18:38 +0100

    fix(webpack): Fixed wrong import clean-webpack-plugin in production config, and removed console.log from webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const webpackMerge = require("webpack-merge");
 const commonConfiguration = require("./webpack/common");
 
 module.exports = (_env, { mode }) => {
-	console.log(mode);
     const enviromentConfig = require(`./webpack/${mode}`);
 	const mergedConfig = webpackMerge(commonConfiguration, enviromentConfig);
 	
