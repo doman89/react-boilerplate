b480b5c9a75c41c6025846039f0723d08eaedb2c
Mateusz Domanski <mateusz.domanski.1989@gmail.com>
2020-02-29 19:16:55 +0100

    fix: Fix all errors from linting in files written before linting rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { default as bemCssModules } from 'bem-css-modules';
-import { Content } from './components/Content/Content'
+import { Content } from './components/Content/Content';
 import { Header } from './components/Header/Header';
 
 import './App.css';
 
 bemCssModules.setSettings({
-    modifierDelimiter: '--',
-    throwOnError: true,
+	modifierDelimiter: '--',
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	throwOnError: true,
 });
 
 export const App: React.FC = () => (
