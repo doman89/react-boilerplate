488ec7ff12c5482364577137937afdb9da297abb
Mateusz Domanski <mateusz.domanski.1989@gmail.com>
2021-06-21 17:54:46 +0200

    refactor: change of whitespaces

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const { merge } = require('webpack-merge');
-const commonConfiguration = require('./webpack/common');     
+const commonConfiguration = require('./webpack/common');
 
 module.exports = (_env, { mode }) => {
   const enviromentConfig = require(`./webpack/${mode}`);
-	const mergedConfig = merge(commonConfiguration, enviromentConfig);
-	
+  const mergedConfig = merge(commonConfiguration, enviromentConfig);
+
   return mergedConfig;
 };
